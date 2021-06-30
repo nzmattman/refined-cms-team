65f866ddc34a265808f2a2a7139beb0369a45bf5
Matthias Law <matthias@refineddigital.co.nz>
2021-06-30 16:26:49 +1200

    fix: fresh install database check

--- src/Module/Providers/TeamServiceProvider.php
+++ src/Module/Providers/TeamServiceProvider.php
@@ -22,13 +22,15 @@ class TeamServiceProvider extends ServiceProvider
             __DIR__.'/../Resources/views',
         ]);
 
-        if ($this->app->runningInConsole()) {
-            if (\DB::connection()->getDatabaseName() && !\Schema::hasTable('teams')) {
-                $this->commands([
-                    Install::class,
-                ]);
+        try {
+            if ($this->app->runningInConsole()) {
+                if (\DB::connection()->getDatabaseName() && !\Schema::hasTable('teams')) {
+                    $this->commands([
+                        Install::class,
+                    ]);
+                }
             }
-        }
+        } catch (\Exception $e) {}
 
         $this->publishes([
             __DIR__.'/../../../config/team.php' => config_path('team.php'),
